7d447be7aec6b5a5608da6d225eaf0278865fb03
chenqinhao <chenqinhao96@qq.com>
2018-08-25 01:15:26 +0800

    theme

---
+++ src/theme/public.ts
@@ -0,0 +1,16 @@
+export { WINDOW } from './src/win.tokens';
+export { preloaderFinished } from './src/services/preloader/preloader';
+export * from './src/services/menu/interface';
+export * from './src/services/menu/menu.service';
+export { ScrollService } from './src/services/scroll/scroll.service';
+export * from './src/services/settings/interface';
+export * from './src/services/settings/settings.service';
+export { TitleService } from './src/services/title/title.service';
+export { COWRY_I18N_TOKEN, CowryI18NService } from './src/services/i18n/i18n';
+export * from './src/services/modal/modal.helper';
+export { _HttpClient } from './src/services/http/http.client';
+export { DatePipe } from './src/pipes/date/date.pipe';
+export { CNCurrencyPipe } from './src/pipes/currency/cn-currency.pipe';
+export { KeysPipe } from './src/pipes/keys/keys.pipe';
+export { YNPipe } from './src/pipes/yn/yn.pipe';
+export { CowryThemeModule } from './src/theme.module';
